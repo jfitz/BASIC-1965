8b8ecea65931990d5240e77404b1df3ca20c84c0
John Fitzpatrick <jfitz@computer.org>
2015-04-05 09:14:54 -0400

    Remove 'Expression' parent class, make its functions global, and simplify constructors for the ValueExpression and TargetExpression classes.

--- bin/Expressions.rb
+++ bin/Expressions.rb
@@ -112,8 +112,7 @@ class List
   def evaluate(interpreter, stack)
     result = Array.new
     @parsed_expressions.each do | parsed_expression |
-      ve = ValueExpression.new(parsed_expression)
-      result << ve.evaluate(interpreter)
+      result << eval(interpreter, parsed_expression, 'NumericConstant')
     end
     result
   end
@@ -229,224 +228,206 @@ class Function
   end
 end
 
-class Expression
-  def initialize(text)
-    raise(Exception, "Expression cannot be empty", caller) if text.length == 0
-    @unparsed_expression = text
-  end
+def split(text)
+  # split the input infix string
+  regex = Regexp.new('([\+\-\*\/\(\)\^])')
+  text.split(regex)
+end
 
-  protected
-  def split(text)
-    # split the input infix string
-    regex = Regexp.new('([\+\-\*\/\(\)\^])')
-    text.split(regex)
-  end
-
-  protected
-  def tokenize(words)
-    tokens = Array.new
-    last_was_operand = false
-    # convert tokens to objects
-    words.each do | word |
-      ## puts "word: #{word}"
-      if word.size > 0 then
-        if word == '(' then
-          tokens << '('
+def tokenize(words)
+  tokens = Array.new
+  last_was_operand = false
+  # convert tokens to objects
+  words.each do | word |
+    ## puts "word: #{word}"
+    if word.size > 0 then
+      if word == '(' then
+        tokens << '('
+        last_was_operand = true
+      else
+        if word == ')'
+          tokens << ')'
           last_was_operand = true
         else
-          if word == ')'
-            tokens << ')'
-            last_was_operand = true
-          else
+          begin
+            if last_was_operand then
+              tokens << BinaryOperator.new(word)
+            else
+              tokens << UnaryOperator.new(word)
+            end
+            last_was_operand = false
+          rescue BASICException
             begin
-              if last_was_operand then
-                tokens << BinaryOperator.new(word)
-              else
-                tokens << UnaryOperator.new(word)
-              end
-              last_was_operand = false
+              tokens << Function.new(word)
+              last_was_operand = true
             rescue BASICException
               begin
-                tokens << Function.new(word)
+                tokens << NumericConstant.new(word)
                 last_was_operand = true
               rescue BASICException
                 begin
-                  tokens << NumericConstant.new(word)
+                  var_name = VariableName.new(word)
+                  tokens << VariableValue.new(var_name)
                   last_was_operand = true
                 rescue BASICException
-                  begin
-                    var_name = VariableName.new(word)
-                    tokens << VariableValue.new(var_name)
-                    last_was_operand = true
-                  rescue BASICException
-                    raise BASICException, "'#{word}' is not a value or operator", caller
-                  end
+                  raise BASICException, "'#{word}' is not a value or operator", caller
                 end
               end
             end
           end
         end
       end
-      ## puts "  tokens: [#{tokens.join('] [')}]"
     end
-    tokens << TerminalOperator.new
+    ## puts "  tokens: [#{tokens.join('] [')}]"
   end
+  tokens << TerminalOperator.new
+end
 
-  protected
-  def parse(tokens)
-    operator_stack = Array.new
-    expression_stack = Array.new
-    parsed_expression = Array.new
+def parse(tokens)
+  operator_stack = Array.new
+  expression_stack = Array.new
+  parsed_expression = Array.new
 
-    ## puts "tokens: [#{tokens.join('] [')}]"
-    last_was_function = false
-    last_was_variable = false
-    # scan the token list from left to right
-    tokens.each do | token |
-      if token != '' then
-        ## puts "token: #{token.class} #{token}"
-        # If the token is a left parenthesis, push it on the operator stack
-        if token == '(' then
-          if last_was_function or last_was_variable then
-            # start parsing the list of function arguments
-            expression_stack.push(parsed_expression)
-            parsed_expression = Array.new
-            operator_stack.push('[')
-          else
-            operator_stack.push(token)
+  ## puts "tokens: [#{tokens.join('] [')}]"
+  last_was_function = false
+  last_was_variable = false
+  # scan the token list from left to right
+  tokens.each do | token |
+    if token != '' then
+      ## puts "token: #{token.class} #{token}"
+      # If the token is a left parenthesis, push it on the operator stack
+      if token == '(' then
+        if last_was_function or last_was_variable then
+          # start parsing the list of function arguments
+          expression_stack.push(parsed_expression)
+          parsed_expression = Array.new
+          operator_stack.push('[')
+        else
+          operator_stack.push(token)
+        end
+        last_was_function = false
+        last_was_variable = false
+      else
+        raise(BASICException, "Function requires parentheses", caller) if last_was_function
+        # If the token is a right parenthesis,
+        # pop the operator stack until the corresponding left parenthesis is removed
+        # Append each operator to the end of the output list
+        if token == ')' then
+          while operator_stack.size > 0 and operator_stack[-1] != '(' and operator_stack[-1] != '[' do
+            op = operator_stack.pop
+            parsed_expression << op
+          end
+          start_op = operator_stack.pop  # remove the '(' or '['
+          if start_op == '[' then
+            pe = Array.new
+            pe << parsed_expression
+            list = List.new(pe)
+            operator_stack.push(list)
+            parsed_expression = expression_stack.pop
           end
           last_was_function = false
           last_was_variable = false
         else
-          raise(BASICException, "Function requires parentheses", caller) if last_was_function
-          # If the token is a right parenthesis,
-          # pop the operator stack until the corresponding left parenthesis is removed
-          # Append each operator to the end of the output list
-          if token == ')' then
-            while operator_stack.size > 0 and operator_stack[-1] != '(' and operator_stack[-1] != '[' do
+          if token.is_operator or token.is_function or token.is_variable then
+            # remove operators already on the stack that have higher or equal precedence
+            # append them to the output list
+            while operator_stack.size > 0 and operator_stack[-1] != '(' and operator_stack[-1] != '[' and operator_stack[-1].precedence >= token.precedence do
               op = operator_stack.pop
               parsed_expression << op
             end
-            start_op = operator_stack.pop  # remove the '(' or '['
-            if start_op == '[' then
-              pe = Array.new
-              pe << parsed_expression
-              list = List.new(pe)
-              operator_stack.push(list)
-              parsed_expression = expression_stack.pop
+            # push the operator onto the operator stack
+            if not token.is_terminal then
+              operator_stack.push(token)
             end
-            last_was_function = false
-            last_was_variable = false
           else
-            if token.is_operator or token.is_function or token.is_variable then
-              # remove operators already on the stack that have higher or equal precedence
-              # append them to the output list
-              while operator_stack.size > 0 and operator_stack[-1] != '(' and operator_stack[-1] != '[' and operator_stack[-1].precedence >= token.precedence do
-                op = operator_stack.pop
-                parsed_expression << op
-              end
-              # push the operator onto the operator stack
-              if not token.is_terminal then
-                operator_stack.push(token)
-              end
-            else
-              # the token is an operand, append it to the output list
-              parsed_expression << token
-            end
-            last_was_function = token.is_function
-            last_was_variable = token.is_variable
+            # the token is an operand, append it to the output list
+            parsed_expression << token
           end
+          last_was_function = token.is_function
+          last_was_variable = token.is_variable
         end
       end
-      ## puts "  OS: [#{operator_stack.join('] [')}]"
-      ## puts "  CE: [#{parsed_expression.join('] [')}]"
     end
-    ## puts "OS: [#{operator_stack.join('] [')}]"
-    ## puts "CE: [#{parsed_expression.join('] [')}]"
-    parsed_expression
+    ## puts "  OS: [#{operator_stack.join('] [')}]"
+    ## puts "  CE: [#{parsed_expression.join('] [')}]"
+  end
+  ## puts "OS: [#{operator_stack.join('] [')}]"
+  ## puts "CE: [#{parsed_expression.join('] [')}]"
+  parsed_expression
+end
+
+def eval(interpreter, parsed_expression, expected_result_class)
+  ## puts "eval CE: [#{@parsed_expression.join('] [')}]"
+  stack = Array.new
+  parsed_expression.each do | token |
+    ## puts "token: #{token.class} #{token}"
+    case token.class.to_s
+    when 'List'
+      x = token.evaluate(interpreter, stack)
+    when 'UnaryOperator','BinaryOperator','Function'
+      x = token.evaluate(interpreter, stack)
+    when 'NumericConstant'
+      x = token.evaluate(interpreter, stack)
+    when 'VariableValue','VariableReference'
+      x = token.evaluate(interpreter, stack)
+    else
+      raise Exception, "Unknown data type #{x.class}", caller
+    end
+    stack.push(x)
+    ## puts "stack: [#{stack.join('] [')}]"
+  end
+  # should be only one item on stack
+  n = stack.length
+  raise(Exception, "Too many items (#{n}) remaining on evaluation stack", caller) if n > 1
+  raise(Exception, "Not enough items (#{n}) remaining on evaluation stack", caller) if n < 1
+  # that is the result
+  item = stack[0]
+  raise(Exception, "Wrong item type (#{item.class}) remaining on evaluation stack", caller) if item.class.to_s != expected_result_class
+  item
+end
+
+class ValueExpression
+  def initialize(text)
+    raise(Exception, "Expression cannot be empty", caller) if text.length == 0
+    @unparsed_expression = text
+
+    words = split(text)
+    ## puts "DBG: words=[#{words.join('] [')}]"
+    tokens = tokenize(words)
+    ## puts "DBG: tokens=[#{tokens.join('] [')}]"
+    @parsed_expression = parse(tokens)
+    ## puts "DBG: CE=[#{@parsed_expression.join('] [')}]"
   end
 
-  protected
-  def eval(interpreter, expected_result_class)
-    ## puts "eval CE: [#{@parsed_expression.join('] [')}]"
-    stack = Array.new
-    @parsed_expression.each do | token |
-      ## puts "token: #{token.class} #{token}"
-      case token.class.to_s
-      when 'List'
-        x = token.evaluate(interpreter, stack)
-      when 'UnaryOperator','BinaryOperator','Function'
-        x = token.evaluate(interpreter, stack)
-      when 'ArgumentCounter'
-        x = token.evaluate(interpreter, stack)
-      when 'NumericConstant'
-        x = token.evaluate(interpreter, stack)
-      when 'VariableValue','VariableReference'
-        x = token.evaluate(interpreter, stack)
-      else
-        raise Exception, "Unknown data type #{x.class}", caller
-      end
-      stack.push(x)
-      ## puts "stack: [#{stack.join('] [')}]"
-    end
-    # should be only one item on stack
-    n = stack.length
-    raise(Exception, "Too many items (#{n}) remaining on evaluation stack", caller) if n > 1
-    raise(Exception, "Not enough items (#{n}) remaining on evaluation stack", caller) if n < 1
-    # that is the result
-    item = stack[0]
-    raise(Exception, "Wrong item type (#{item.class}) remaining on evaluation stack", caller) if item.class.to_s != expected_result_class
-    item
-  end
-  
-  public
   def to_s
     @unparsed_expression
   end
-end
-
-class ValueExpression < Expression
-  def initialize(text)
-    if text.class.to_s == 'String' then
-      super
-
-      words = split(text)
-      ## puts "DBG: words=[#{words.join('] [')}]"
-      tokens = tokenize(words)
-      ## puts "DBG: tokens=[#{tokens.join('] [')}]"
-      @parsed_expression = parse(tokens)
-      ## puts "DBG: CE=[#{@parsed_expression.join('] [')}]"
-    else
-      @unparsed_expression = text
-      @parsed_expression = text
-    end
-  end
 
   def evaluate(interpreter)
-    eval(interpreter, 'NumericConstant')
+    eval(interpreter, @parsed_expression, 'NumericConstant')
   end
 end
 
-class TargetExpression < Expression
+class TargetExpression
   def initialize(text)
-    if text.class.to_s == 'String' then
-      super
+    raise(Exception, "Expression cannot be empty", caller) if text.length == 0
+    @unparsed_expression = text
 
-      words = split(text)
-      ## puts "DBG: words=[#{words.join('] [')}]"
-      tokens = tokenize(words)
-      @parsed_expression = parse(tokens)
-      raise(BASICException, "Value is not assignable (length 0)", caller) if @parsed_expression.length == 0
-      raise(BASICException, "Value is not assignable (type #{@parsed_expression[-1].class})", caller) if @parsed_expression[-1].class.to_s != 'VariableValue'
-      @parsed_expression[-1] = VariableReference.new(@parsed_expression[-1])
-    else
-      @unparsed_expression = text
-      @parsed_expression = text
-    end
+    words = split(text)
+    ## puts "DBG: words=[#{words.join('] [')}]"
+    tokens = tokenize(words)
+    @parsed_expression = parse(tokens)
+    raise(BASICException, "Value is not assignable (length 0)", caller) if @parsed_expression.length == 0
+    raise(BASICException, "Value is not assignable (type #{@parsed_expression[-1].class})", caller) if @parsed_expression[-1].class.to_s != 'VariableValue'
+    @parsed_expression[-1] = VariableReference.new(@parsed_expression[-1])
+  end
+
+  def to_s
+    @unparsed_expression
   end
 
   def evaluate(interpreter)
-    eval(interpreter, 'String')
+    eval(interpreter, @parsed_expression, 'String')
   end
 end
 
