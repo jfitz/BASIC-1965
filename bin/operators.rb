6a8c282f8aab7675a3eec427d37859ea21fb818b
John Fitzpatrick <jfitz@computer.org>
2016-01-25 19:01:06 -0500

    Add matrix multiplication operation and test cases.

--- bin/operators.rb
+++ bin/operators.rb
@@ -504,6 +504,16 @@ class BinaryOperator < AbstractToken
     values
   end
 
+  def add_matrix_matrix(a, b)
+    # verify dimensions match
+    a_dims = a.dimensions
+    b_dims = b.dimensions
+    fail(BASICException, 'Matrix dimensions do not match') if a_dims != b_dims
+    values = add_matrix_matrix_1(a, b) if a_dims.size == 1
+    values = add_matrix_matrix_2(a, b) if a_dims.size == 2
+    Matrix.new(a_dims, values)
+  end
+
   def subtract_matrix_matrix_1(a, b)
     a_dims = a.dimensions
     values = {}
@@ -539,51 +549,6 @@ class BinaryOperator < AbstractToken
     values
   end
 
-  def multiply_matrix_matrix_1(a, b)
-    a_dims = a.dimensions
-    values = {}
-    n_cols = a_dims[0].to_i
-    values = {}
-    (1..n_cols).each do |col|
-      a_value = a.get_value_1(col)
-      b_value = b.get_value_1(col)
-      f = a_value.to_f + b_value.to_f
-      f2 = float_to_possible_int(f)
-      coords = '(' + col.to_s + ')'
-      values[coords] = NumericConstant.new(f2)
-    end
-    values
-  end
-
-  def multiply_matrix_matrix_2(a, b)
-    a_dims = a.dimensions
-    values = {}
-    n_rows = a_dims[0].to_i
-    n_cols = a_dims[1].to_i
-    values = {}
-    (1..n_rows).each do |row|
-      (1..n_cols).each do |col|
-        a_value = a.get_value_2(row, col)
-        b_value = b.get_value_2(row, col)
-        f = a_value.to_f + b_value.to_f
-        f2 = float_to_possible_int(f)
-        coords = '(' + row.to_s + ',' + col.to_s + ')'
-        values[coords] = NumericConstant.new(f2)
-      end
-    end
-    values
-  end
-
-  def add_matrix_matrix(a, b)
-    # verify dimensions match
-    a_dims = a.dimensions
-    b_dims = b.dimensions
-    fail(BASICException, 'Matrix dimensions do not match') if a_dims != b_dims
-    values = add_matrix_matrix_1(a, b) if a_dims.size == 1
-    values = add_matrix_matrix_2(a, b) if a_dims.size == 2
-    Matrix.new(a_dims, values)
-  end
-
   def subtract_matrix_matrix(a, b)
     # verify dimensions match
     a_dims = a.dimensions
@@ -594,34 +559,41 @@ class BinaryOperator < AbstractToken
     Matrix.new(a_dims, values)
   end
 
-  def multiply_matrix_matrix_w(a, b)
-    # do something
-  end
-
-  def multiply_vector_matrix(a, b)
-    # do something
-  end
-
-  def multiply_matrix_vector(a, b)
-    # do something
+  def multiply_matrix_matrix_w(a, b, r_dims)
+    r_rows = r_dims[0].to_i
+    r_cols = r_dims[1].to_i
+    a_dims = a.dimensions
+    a_rows = a_dims[0].to_i
+    a_cols = a_dims[1].to_i
+    values = {}
+    (1..r_rows).each do |r_row|
+      (1..r_cols).each do |r_col|
+        f = 0
+        (1..a_cols).each do |a_col|
+          a_value = a.get_value_2(r_row, a_col)
+          b_value = b.get_value_2(a_col, r_col)
+          f += a_value.to_f * b_value.to_f
+        end
+        f2 = float_to_possible_int(f)
+        coords = '(' + r_row.to_s + ',' + r_col.to_s + ')'
+        values[coords] = NumericConstant.new(f2)
+      end
+    end
+    values
   end
 
   def multiply_matrix_matrix(a, b)
     # verify dimensions are acceptable
     a_dims = a.dimensions
     b_dims = b.dimensions
-    # matrix/matrix
-    fail(BASICException, 'Matrix dimensions do not match') if a_dims[1] != b_dims[0]
+    fail(BASICException, 'Matrix multiplication must have two matrices') if
+      a_dims.size != 2 || b_dims.size != 2
+    # number of columns in a must match number of rows in b
+    fail(BASICException, 'Matrix dimensions do not match') if
+      a_dims[1] != b_dims[0]
+    # result has number of rows in a and number of columns in b
     r_dims = [a_dims[0], b_dims[1]]
-    values = multiply_matrix_matrix_w(a, b)
-    # vector/matrix
-    fail(BASICException, 'Matrix dimensions do not match') if a_dims[0] != b_dims[0]
-    r_dims = [a_dims[0], b_dims[1]]
-    values = multiply_vector_matrix(a, b)
-    # matrix/vector
-    fail(BASICException, 'Matrix dimensions do not match') if a_dims[1] != b_dims[0]
-    r_dims = [a_dims[0], b_dims[0]]
-    values = multiply_matrix_vector(a, b)
+    values = multiply_matrix_matrix_w(a, b, r_dims)
     Matrix.new(r_dims, values)
   end
 
--- test/bin/run_unit_tests.sh
+++ test/bin/run_unit_tests.sh
@@ -232,6 +232,19 @@ bash test/bin/run_one_test.sh mat_let_uni_minus
 bash test/bin/list_one_test.sh mat_let_uni_minus
 ((ECODE+=$?))
 
+bash test/bin/run_one_test.sh mat_add_m_m
+((ECODE+=$?))
+bash test/bin/list_one_test.sh mat_add_m_m
+((ECODE+=$?))
+bash test/bin/run_one_test.sh mat_subtract_m_m
+((ECODE+=$?))
+bash test/bin/list_one_test.sh mat_subtract_m_m
+((ECODE+=$?))
+bash test/bin/run_one_test.sh mat_multiply_m_m
+((ECODE+=$?))
+bash test/bin/list_one_test.sh mat_multiply_m_m
+((ECODE+=$?))
+
 bash test/bin/run_one_test.sh mat_add_s_m
 ((ECODE+=$?))
 bash test/bin/list_one_test.sh mat_add_s_m
