d834511419c335eee5ae63f11f1adc6ac2ebaaf5
John Fitzpatrick <jfitz@computer.org>
2017-03-06 18:42:32 -0800

    Combine RemStatement and RemarkStatement classes.

--- bin/statements.rb
+++ bin/statements.rb
@@ -97,9 +97,9 @@ class StatementFactory
   def create(text)
     squeezed = squeeze_out_spaces(text)
     return Line.new(text, EmptyStatement.new, []) if squeezed == ''
-    return Line.new(text, RemarkStatement.new(text), []) if
+    return Line.new(text, RemarkStatement.new('REMARK', text), []) if
       squeezed[0..5] == 'REMARK'
-    return Line.new(text, RemStatement.new(text), []) if
+    return Line.new(text, RemarkStatement.new('REM',text), []) if
       squeezed[0..2] == 'REM'
 
     tokens = tokenize(squeezed)
@@ -286,29 +286,12 @@ end
 
 # REMARK
 class RemarkStatement < AbstractStatement
-  def initialize(line)
+  def initialize(keyword, line)
+    super(keyword, line)
     # override the method to squeeze spaces from line
     squeezed = line.strip
-    super('REMARK', line)
-    @rest = squeezed[6..-1]
-  end
-
-  def to_s
-    @keyword + @rest
-  end
-
-  def execute(_, _)
-    0
-  end
-end
-
-# REM
-class RemStatement < AbstractStatement
-  def initialize(line)
-    # override the method to squeeze spaces from line
-    squeezed = line.strip
-    super('REM', line)
-    @rest = squeezed[3..-1]
+    index = keyword.size
+    @rest = squeezed[index..-1]
   end
 
   def to_s
