20517298024dc5cf1c6799ef9a1b1e9646be9834
John Fitzpatrick <jfitz@computer.org>
2017-07-11 18:49:00 -0700

    Move program-related commands out of Interpreter and into Shell.

--- bin/basic.rb
+++ bin/basic.rb
@@ -212,7 +212,6 @@ class Interpreter
     @running = false
     @randomizer = Random.new(1)
     @randomizer = Random.new if randomize
-    @statement_factory = StatementFactory.new
     @int_floor = int_floor
     @ignore_rnd_arg = ignore_rnd_arg
     @console_io =
@@ -229,157 +228,23 @@ class Interpreter
     @program_lines = {}
   end
 
-  def parse_line(line)
-    @statement_factory.parse(line)
-  rescue BASICException => e
-    @console_io.print_line("Syntax error: #{e.message}")
-  end
-
-  def cmd_list(linespec, list_tokens)
-    linespec = linespec.strip
-    if !@program_lines.empty?
-      begin
-        line_numbers = @program_lines.keys.sort
-        line_number_range = LineListSpec.new(linespec, line_numbers)
-        line_numbers = line_number_range.line_numbers
-        list_lines_errors(line_numbers, list_tokens)
-      rescue BASICException => e
-        @console_io.print_line(e)
-      end
-    else
-      @console_io.print_line('No program loaded')
-    end
-  end
-
-  def cmd_pretty(linespec)
-    linespec = linespec.strip
-    if !@program_lines.empty?
-      begin
-        line_numbers = @program_lines.keys.sort
-        line_number_range = LineListSpec.new(linespec, line_numbers)
-        line_numbers = line_number_range.line_numbers
-        pretty_lines_errors(line_numbers)
-      rescue BASICException => e
-        @console_io.print_line(e)
-      end
-    else
-      @console_io.print_line('No program loaded')
-    end
-  end
-
-  def cmd_profile(linespec)
-    linespec = linespec.strip
-    if !@program_lines.empty?
-      begin
-        line_numbers = @program_lines.keys.sort
-        line_number_range = LineListSpec.new(linespec, line_numbers)
-        line_numbers = line_number_range.line_numbers
-        profile_lines_errors(line_numbers)
-      rescue BASICException => e
-        @console_io.print_line(e)
-      end
-    else
-      @console_io.print_line('No program loaded')
-    end
-  end
-
   private
-
-  def list_lines_errors(line_numbers, list_tokens)
-    line_numbers.each do |line_number|
-      line = @program_lines[line_number]
-      @console_io.print_line(line_number.to_s + line.list)
-      statement = line.statement
-      statement.errors.each { |error| puts ' ' + error }
-      tokens = line.tokens
-      text_tokens = tokens.map(&:to_s)
-      @console_io.print_line('TOKENS: ' + text_tokens.to_s) if list_tokens
-    end
-  end
-
-  def pretty_lines_errors(line_numbers)
-    line_numbers.each do |line_number|
-      line = @program_lines[line_number]
-      number = line_number.to_s
-      pretty = line.pretty
-      @console_io.print_line(number + pretty)
-      statement = line.statement
-      statement.errors.each { |error| puts ' ' + error }
-    end
-  end
-
-  def profile_lines_errors(line_numbers)
-    line_numbers.each do |line_number|
-      line = @program_lines[line_number]
-      number = line_number.to_s
-      profile = line.profile
-      @console_io.print_line(number + profile)
-      statement = line.statement
-      statement.errors.each { |error| puts ' ' + error }
-    end
-  end
-
-  def list_lines(line_numbers)
-    line_numbers.each do |line_number|
-      line = @program_lines[line_number]
-      text = line.list
-      @console_io.print_line(line_number.to_s + text)
-    end
-  end
-
-  def delete_specific_lines(line_numbers)
-    line_numbers.each { |line_number| @program_lines.delete(line_number) }
-  end
-
-  def list_and_delete_lines(line_numbers)
-    list_lines(line_numbers)
-    print 'DELETE THESE LINES? '
-    answer = @console_io.read_line
-    delete_specific_lines(line_numbers) if answer == 'YES'
-  end
-
-  public
-
-  def cmd_delete(linespec)
-    if @program_lines.empty?
-      @console_io.print_line('No program loaded')
-    else
-      delete_lines(linespec.strip)
-    end
-  end
-
-  private
-
-  def delete_lines(linespec)
-    line_numbers = @program_lines.keys.sort
-    line_number_range = LineListSpec.new(linespec.strip, line_numbers)
-    line_numbers = line_number_range.line_numbers
-    case line_number_range.range_type
-    when :single
-      delete_specific_lines(line_numbers)
-    when :range
-      list_and_delete_lines(line_numbers)
-    when :all
-      @console_io.print_line('Type NEW to delete an entire program')
-    end
-  rescue BASICException => e
-    @console_io.print_line(e)
-  end
 
   def verify_next_line_number
     raise BASICException, 'Program terminated without END' if
       @next_line_number.nil?
     line_numbers = @program_lines.keys
-    raise BASICException, "Line number #{@next_line_number} not found" unless
+    raise(BASICException,
+          "Line number #{@next_line_number} not found") unless
       line_numbers.include?(@next_line_number)
   end
 
   public
 
-  def cmd_run(trace_flag)
-    @variables = {}
-    @tron_flag = false
+  def run(program_lines, trace_flag)
+    @program_lines = program_lines
 
+    # reset profile metrics
     @program_lines.keys.sort.each do |line_number|
       line = @program_lines[line_number]
       statement = line.statement
@@ -387,16 +252,12 @@ class Interpreter
       statement.profile_time = 0
     end
 
-    if @program_lines.empty?
-      @console_io.print_line('No program loaded')
-    else
-      @running = true
-      run_phase_1
-      run_phase_2(trace_flag) if @running
-    end
+    # run the program
+    @tron_flag = false
+    @running = true
+    run_phase_1
+    run_phase_2(trace_flag) if @running
   end
-
-  private
 
   def run_phase_1
     # phase 1: do all initialization (store values in DATA lines)
@@ -505,60 +366,8 @@ class Interpreter
     @tron_flag = tron_flag
   end
 
-  def cmd_new
-    @program_lines = {}
+  def clear_variables
     @variables = {}
-  end
-
-  def cmd_load(filename, trace_flag)
-    filename = filename.strip
-    if !filename.empty?
-      begin
-        File.open(filename, 'r') do |file|
-          @program_lines = {}
-          file.each_line do |line|
-            line = @console_io.ascii_printables(line)
-            @console_io.print_line(line) if trace_flag
-            store_program_line(line, false)
-          end
-        end
-        true
-      rescue Errno::ENOENT
-        @console_io.print_line("File '#{filename}' not found")
-        false
-      end
-    else
-      @console_io.print_line('Filename not specified')
-      false
-    end
-  end
-
-  def cmd_save(filename)
-    if @program_lines.empty?
-      @console_io.print_line('No program loaded')
-    else
-      filename = filename.strip
-      if filename.empty?
-        @console_io.print_line('Filename not specified')
-      else
-        save_file(filename)
-      end
-    end
-  end
-
-  def save_file(filename)
-    line_numbers = @program_lines.keys.sort
-    begin
-      File.open(filename, 'w') do |file|
-        line_numbers.each do |line_num|
-          text = @program_lines[line_num]
-          file.puts line_num + ' ' + text
-        end
-        file.close
-      end
-    rescue Errno::ENOENT
-      @console_io.print_line("File '#{filename}' not opened")
-    end
   end
 
   # returns an Array of values
@@ -797,19 +606,56 @@ class Interpreter
     fh
   end
 
-  def store_program_line(cmd, print_errors)
-    line_num, line = parse_line(cmd)
-    if !line_num.nil? && !line.nil?
-      check_line_duplicate(line_num, print_errors)
-      check_line_sequence(line_num, print_errors)
-      @program_lines[line_num] = line
-      statement = line.statement
-      statement.errors.each { |error| puts error } if print_errors
-      !statement.errors.empty?
-    else
-      true
+  def int_floor?
+    @int_floor
+  end
+end
+
+class Shell
+  def initialize(interpreter)
+    @interpreter = interpreter
+    @console_io = interpreter.console_io
+    @program_lines = {}
+    @statement_factory = StatementFactory.new
+  end
+
+  def run(interpreter)
+    need_prompt = true
+    done = false
+    until done
+      @console_io.print_line('READY') if need_prompt
+      cmd = @console_io.read_line
+      if /\A\d/ =~ cmd
+        # starts with a number, so maybe it is a program line
+        need_prompt = store_program_line(cmd, true)
+      else
+        # immediate command -- execute
+        done = execute_command(cmd)
+        need_prompt = true
+      end
     end
   end
+
+  def load_and_run(filename, trace_flag, timing_flag, profile_flag)
+    @program_lines = {}
+    return unless cmd_load(filename, false)
+
+    timing = Benchmark.measure { cmd_run(trace_flag) }
+    print_timing(timing) if timing_flag
+    print_profile if profile_flag
+  end
+
+  def load_and_list(filename, trace_flag, list_tokens)
+    @program_lines = {}
+    cmd_list('', list_tokens) if cmd_load(filename, trace_flag)
+  end
+
+  def load_and_pretty(filename, trace_flag)
+    @program_lines = {}
+    cmd_pretty('') if cmd_load(filename, trace_flag)
+  end
+
+  private
 
   def execute_command(cmd)
     return false if cmd.empty?
@@ -829,24 +675,6 @@ class Interpreter
       print "Unknown command #{cmd}\n"
     end
     false
-  end
-
-  private
-
-  def check_line_duplicate(line_num, print_errors)
-    # warn about duplicate lines when loading
-    # but not when typing
-    @console_io.print_line("Duplicate line number #{line_num}") if
-      @program_lines.key?(line_num) && !print_errors
-  end
-
-  def check_line_sequence(line_num, print_errors)
-    # warn about lines out of sequence
-    # but not when typing
-    @console_io.print_line("Line #{line_num} is out of sequence") if
-      !@program_lines.empty? &&
-      line_num < @program_lines.max[0] &&
-      !print_errors
   end
 
   def simple_command?(text)
@@ -911,12 +739,136 @@ class Interpreter
     end
   end
 
-  public
+  def cmd_new
+    @program_lines = {}
+    @interpreter.clear_variables
+  end
 
   def execute_run_command
     timing = Benchmark.measure { cmd_run(false) }
     print_timing(timing)
     @console_io.print_line('')
+  end
+
+  def cmd_run(trace_flag)
+    @interpreter.clear_variables
+
+    if @program_lines.empty?
+      @console_io.print_line('No program loaded')
+    else
+      @interpreter.run(@program_lines, trace_flag)
+    end
+  end
+  
+  def dump_vars
+    @interpreter.dump_vars
+  end
+
+  def dump_user_functions
+    @interpreter.dump_user_functions
+  end
+
+  def dump_dims
+    @interpreter.dump_dims
+  end
+
+  def cmd_list(linespec, list_tokens)
+    linespec = linespec.strip
+    if !@program_lines.empty?
+      begin
+        line_numbers = @program_lines.keys.sort
+        line_number_range = LineListSpec.new(linespec, line_numbers)
+        line_numbers = line_number_range.line_numbers
+        list_lines_errors(line_numbers, list_tokens)
+      rescue BASICException => e
+        @console_io.print_line(e)
+      end
+    else
+      @console_io.print_line('No program loaded')
+    end
+  end
+
+  def cmd_pretty(linespec)
+    linespec = linespec.strip
+    if !@program_lines.empty?
+      begin
+        line_numbers = @program_lines.keys.sort
+        line_number_range = LineListSpec.new(linespec, line_numbers)
+        line_numbers = line_number_range.line_numbers
+        pretty_lines_errors(line_numbers)
+      rescue BASICException => e
+        @console_io.print_line(e)
+      end
+    else
+      @console_io.print_line('No program loaded')
+    end
+  end
+
+  def cmd_profile(linespec)
+    linespec = linespec.strip
+    if !@program_lines.empty?
+      begin
+        line_numbers = @program_lines.keys.sort
+        line_number_range = LineListSpec.new(linespec, line_numbers)
+        line_numbers = line_number_range.line_numbers
+        profile_lines_errors(line_numbers)
+      rescue BASICException => e
+        @console_io.print_line(e)
+      end
+    else
+      @console_io.print_line('No program loaded')
+    end
+  end
+
+  def cmd_load(filename, trace_flag)
+    filename = filename.strip
+    if !filename.empty?
+      begin
+        File.open(filename, 'r') do |file|
+          @program_lines = {}
+          file.each_line do |line|
+            line = @console_io.ascii_printables(line)
+            @console_io.print_line(line) if trace_flag
+            store_program_line(line, false)
+          end
+        end
+        true
+      rescue Errno::ENOENT
+        @console_io.print_line("File '#{filename}' not found")
+        false
+      end
+    else
+      @console_io.print_line('Filename not specified')
+      false
+    end
+  end
+
+  def cmd_save(filename)
+    if @program_lines.empty?
+      @console_io.print_line('No program loaded')
+    else
+      filename = filename.strip
+      if filename.empty?
+        @console_io.print_line('Filename not specified')
+      else
+        save_file(filename)
+      end
+    end
+  end
+
+  def save_file(filename)
+    line_numbers = @program_lines.keys.sort
+    begin
+      File.open(filename, 'w') do |file|
+        line_numbers.each do |line_num|
+          text = @program_lines[line_num]
+          file.puts line_num + ' ' + text
+        end
+        file.close
+      end
+    rescue Errno::ENOENT
+      @console_io.print_line("File '#{filename}' not opened")
+    end
   end
 
   def print_profile
@@ -929,25 +881,6 @@ class Interpreter
     end
   end
 
-  def load_and_run(filename, trace_flag, timing_flag, profile_flag)
-    @program_lines = {}
-    return unless cmd_load(filename, false)
-
-    timing = Benchmark.measure { cmd_run(trace_flag) }
-    print_timing(timing) if timing_flag
-    print_profile if profile_flag
-  end
-
-  def load_and_list(filename, trace_flag, list_tokens)
-    @program_lines = {}
-    cmd_list('', list_tokens) if cmd_load(filename, trace_flag)
-  end
-
-  def load_and_pretty(filename, trace_flag)
-    @program_lines = {}
-    cmd_pretty('') if cmd_load(filename, trace_flag)
-  end
-
   def print_timing(timing)
     user_time = timing.utime + timing.cutime
     sys_time = timing.stime + timing.cstime
@@ -956,29 +889,118 @@ class Interpreter
     @console_io.print_line(" user: #{user_time.round(2)}")
     @console_io.print_line(" system: #{sys_time.round(2)}")
   end
-
-  def int_floor?
-    @int_floor
-  end
-end
-
-class Shell
-  def run(interpreter)
-    console_io = interpreter.console_io
-    need_prompt = true
-    done = false
-    until done
-      console_io.print_line('READY') if need_prompt
-      cmd = console_io.read_line
-      if /\A\d/ =~ cmd
-        # starts with a number, so maybe it is a program line
-        need_prompt = interpreter.store_program_line(cmd, true)
-      else
-        # immediate command -- execute
-        done = interpreter.execute_command(cmd)
-        need_prompt = true
-      end
+  
+  def cmd_delete(linespec)
+    if @program_lines.empty?
+      @console_io.print_line('No program loaded')
+    else
+      delete_lines(linespec.strip)
     end
+  end
+
+  def delete_lines(linespec)
+    line_numbers = @program_lines.keys.sort
+    line_number_range = LineListSpec.new(linespec.strip, line_numbers)
+    line_numbers = line_number_range.line_numbers
+    case line_number_range.range_type
+    when :single
+      delete_specific_lines(line_numbers)
+    when :range
+      list_and_delete_lines(line_numbers)
+    when :all
+      @console_io.print_line('Type NEW to delete an entire program')
+    end
+  rescue BASICException => e
+    @console_io.print_line(e)
+  end
+
+  def store_program_line(cmd, print_errors)
+    line_num, line = parse_line(cmd)
+    if !line_num.nil? && !line.nil?
+      check_line_duplicate(line_num, print_errors)
+      check_line_sequence(line_num, print_errors)
+      @program_lines[line_num] = line
+      statement = line.statement
+      statement.errors.each { |error| puts error } if print_errors
+      !statement.errors.empty?
+    else
+      true
+    end
+  end
+
+  def parse_line(line)
+    @statement_factory.parse(line)
+  rescue BASICException => e
+    @console_io.print_line("Syntax error: #{e.message}")
+  end
+
+  def check_line_duplicate(line_num, print_errors)
+    # warn about duplicate lines when loading
+    # but not when typing
+    @console_io.print_line("Duplicate line number #{line_num}") if
+      @program_lines.key?(line_num) && !print_errors
+  end
+
+  def check_line_sequence(line_num, print_errors)
+    # warn about lines out of sequence
+    # but not when typing
+    @console_io.print_line("Line #{line_num} is out of sequence") if
+      !@program_lines.empty? &&
+      line_num < @program_lines.max[0] &&
+      !print_errors
+  end
+
+  def list_lines_errors(line_numbers, list_tokens)
+    line_numbers.each do |line_number|
+      line = @program_lines[line_number]
+      @console_io.print_line(line_number.to_s + line.list)
+      statement = line.statement
+      statement.errors.each { |error| puts ' ' + error }
+      tokens = line.tokens
+      text_tokens = tokens.map(&:to_s)
+      @console_io.print_line('TOKENS: ' + text_tokens.to_s) if list_tokens
+    end
+  end
+
+  def pretty_lines_errors(line_numbers)
+    line_numbers.each do |line_number|
+      line = @program_lines[line_number]
+      number = line_number.to_s
+      pretty = line.pretty
+      @console_io.print_line(number + pretty)
+      statement = line.statement
+      statement.errors.each { |error| puts ' ' + error }
+    end
+  end
+
+  def profile_lines_errors(line_numbers)
+    line_numbers.each do |line_number|
+      line = @program_lines[line_number]
+      number = line_number.to_s
+      profile = line.profile
+      @console_io.print_line(number + profile)
+      statement = line.statement
+      statement.errors.each { |error| puts ' ' + error }
+    end
+  end
+
+  def list_lines(line_numbers)
+    line_numbers.each do |line_number|
+      line = @program_lines[line_number]
+      text = line.list
+      @console_io.print_line(line_number.to_s + text)
+    end
+  end
+
+  def delete_specific_lines(line_numbers)
+    line_numbers.each { |line_number| @program_lines.delete(line_number) }
+  end
+
+  def list_and_delete_lines(line_numbers)
+    list_lines(line_numbers)
+    print 'DELETE THESE LINES? '
+    answer = @console_io.read_line
+    delete_specific_lines(line_numbers) if answer == 'YES'
   end
 end
 
@@ -1035,14 +1057,14 @@ if show_heading
   puts
 end
 
+shell = Shell.new(interpreter)
 if !run_filename.nil?
-  interpreter.load_and_run(run_filename, trace_flag, show_timing, profile_flag)
+  shell.load_and_run(run_filename, trace_flag, show_timing, profile_flag)
 elsif !list_filename.nil?
-  interpreter.load_and_list(list_filename, trace_flag, list_tokens)
+  shell.load_and_list(list_filename, trace_flag, list_tokens)
 elsif !pretty_filename.nil?
-  interpreter.load_and_pretty(pretty_filename, trace_flag)
+  shell.load_and_pretty(pretty_filename, trace_flag)
 else
-  shell = Shell.new
   shell.run(interpreter)
 end
 
