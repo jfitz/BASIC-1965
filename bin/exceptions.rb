ba1d7632ff33b80457c0ee825f9865cf980c902f
John Fitzpatrick <jfitz@computer.org>
2020-08-10 18:40:04 -0700

    Add numeric code to trappable errors.

--- bin/exceptions.rb
+++ bin/exceptions.rb
@@ -2,8 +2,20 @@ class BASICError < RuntimeError; end
 
 class BASICCommandError < BASICError; end
 
+class BASICSyntaxError < BASICError; end
+
+class BASICExpressionError < BASICSyntaxError; end
+
+class BASICPreexecuteError < BASICError; end
+
 class BASICRuntimeError < BASICError; end
 
-class BASICExpressionError < BASICError; end
+class BASICIOError < BASICRuntimeError; end
 
-class BASICSyntaxError < BASICError; end
+class BASICValueError < BASICRuntimeError; end
+
+class BASICFunctionError < BASICRuntimeError; end
+
+class BASICStatementError < BASICRuntimeError; end
+
+class BASICVariableError < BASICRuntimeError; end
